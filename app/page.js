74c5a356c8442a3b48667e231e58ff14789f072a
Skyoden <nickmati@gmail.com>
2025-12-30 16:11:16 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Battle Royale</h1>
+      <p>La web del juego está funcionando.</p>
+      <p>Próximo paso: login y tablero.</p>
+    </main>
+  );
+}
